acd8c66376d5bd8ab1ecd5ccff632532203c5edd
Bohdan Boklah <b.boklag@gmail.com>
2025-09-15 00:04:54 +0300

    upd global_config

---
+++ src/global_config.py
@@ -0,0 +1,12 @@
+
+GOOUGLE_SEARCH_TOOL_NAME        = 'Google search'
+GOOUGLE_SEARCH_TOOL_DESCRIPTION = "Asynchronous tool for searching general information on the Internet and returning relevant results."
+NUM_SEARCH_RESULT               = 1
+
+
+TOOLS_AVAILABLE = [
+    {
+        "name": GOOUGLE_SEARCH_TOOL_NAME,
+        "description": GOOUGLE_SEARCH_TOOL_DESCRIPTION,
+    }
+]
