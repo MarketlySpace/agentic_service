e0a79d3eb910049f7966066137b5e5404cf34086
Bohdan Boklah <b.boklag@gmail.com>
2025-09-16 18:45:18 +0300

    add configs for vector DB

--- src/global_config.py
+++ src/global_config.py
@@ -1,4 +1,5 @@
 
+
 GOOUGLE_SEARCH_TOOL_NAME        = 'Google search'
 GOOUGLE_SEARCH_TOOL_DESCRIPTION = "Synchronous tool for searching general information on the Internet and returning relevant results."
 NUM_SEARCH_RESULT               = 1
@@ -12,12 +13,17 @@ TOOLS_AVAILABLE = [
 ]
 
 AVAILABLE_MODEL_URL    = "https://openrouter.ai/api/v1"
-TEMPERATURE_ROUTER = 0.4
+TEMPERATURE_ROUTER     = 0.4
 
 
-
-
-
+VECTOR_DB_DIR        = 'src\db'
+EMBEDDER_CHUNK_SIZE  = 3
+RETRY_REQUEST_TIME   = 15
+TIMEOUT              = 60
+NAME_EMBEDDER_MODEL  = "text-embedding-3-small"
+SPLITER_CHUNK_SIZE   = 512
+SPLITER_OVERLAP      = 100
+K_SAMPLES            = 3
 
 
 
