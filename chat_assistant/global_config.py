874529031a816a3ea6e621d87f8cc5f6f7290419
Bohdan Boklah <b.boklag@gmail.com>
2025-09-21 16:39:24 +0300

    upd: add config for rag router

--- chat_assistant/global_config.py
+++ chat_assistant/global_config.py
@@ -16,14 +16,19 @@ AVAILABLE_MODEL_URL    = "https://openrouter.ai/api/v1"
 TEMPERATURE_ROUTER     = 0.4
 
 
-VECTOR_DB_DIR        = 'src\db'
-EMBEDDER_CHUNK_SIZE  = 3
-RETRY_REQUEST_TIME   = 15
-TIMEOUT              = 60
-NAME_EMBEDDER_MODEL  = "text-embedding-3-small"
-SPLITER_CHUNK_SIZE   = 512
-SPLITER_OVERLAP      = 100
-K_SAMPLES            = 3
+VECTOR_DB_DIR           = 'src\db'
+EMBEDDER_CHUNK_SIZE     = 3
+RETRY_REQUEST_TIME      = 15
+TIMEOUT                 = 60
+NAME_EMBEDDER_MODEL     = "text-embedding-3-small"
+SPLITER_CHUNK_SIZE      = 512
+SPLITER_OVERLAP         = 100
+K_SAMPLES               = 3
+TEMPERATURE             = 0
+LOWER_BOUND_TEMPERATURE = 0
+UPPER_BOUND_TEMPERATURE = 1
+LOWER_BOUND_K_SAMPLES   = 1
+UPPER_BOUND_K_SAMPLES   = 10
 
 
 
